971c42cc979b18f012142743859e245a339f9a5f
salimM21 <salim.majide.officiel@gmail.com>
2025-09-06 09:44:46 +0100

    gradient_boosting_model.pkl

---
+++ src/obesitrack/db/database.py
@@ -0,0 +1,33 @@
+import pandas as pd
+import os
+from sqlalchemy import create_engine, text
+from dotenv import load_dotenv
+import urllib.parse
+import os
+from sqlalchemy.orm import sessionmaker, declarative_base
+
+
+# Charger les variables d'environnement depuis le fichier .env
+load_dotenv()
+
+# Récupérer les variables
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
+
+# Encoder le mot de passe pour l'URL
+# password_encoded = urllib.parse.quote_plus(DB_PASSWORD)
+
+# Créer la chaîne de connexion PostgreSQL
+DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+# Créer l'engine SQLAlchemy
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+
+# Création de la table si elle n’existe pas
+
+
